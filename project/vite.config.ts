b190ca241b85cac8c06f44bf9774f839374afa5d
CHRISTIAN VAZQUEZ <cvazquezherrera1@gmail.com>
2025-02-08 00:39:35 -0600

    Actualización en vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/redes/', // Reemplaza 'redes' con el nombre de tu repositorio en GitHub
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
