ce33b87867a42767512fa67406423bc7f8a6e306
Chriseguro <cvazquezherrera1@gmail.com>
2025-02-08 21:56:38 -0600

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/redes/', // Cambia esto según tu repositorio
+  base: '/redes/', // Redes
 });
 
